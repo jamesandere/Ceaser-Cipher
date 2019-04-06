1d2ed44ed9e7aba0595a3aeee85a6639647f4c09
jamesandere <jamesandere56@gmail.com>
2019-04-06 15:36:55 +0300

    Create encryption test

--- src/test/java/CaesarTest.java
+++ src/test/java/CaesarTest.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CaesarTest {
+  @Test
+  public void Caeser_testEncryption(){
+    Caesar caesar = new Caesar();
+    assertEquals("car", caesar.encrypt("dbs", 1));
+  }
 }
