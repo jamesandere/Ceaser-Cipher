cd20f98a84488843dc02bd2ffa6fbc74a8f183bc
jamesandere <jamesandere56@gmail.com>
2019-04-06 15:54:17 +0300

    Build

--- src/test/java/CaesarTest.java
+++ src/test/java/CaesarTest.java
@@ -5,6 +5,6 @@ public class CaesarTest {
   @Test
   public void Caeser_testEncryption(){
     Caesar caesar = new Caesar();
-    assertEquals("car", caesar.encrypt("dbs", 1));
+    assertEquals("dbs", caesar.encrypt("car", 1));
   }
 }
