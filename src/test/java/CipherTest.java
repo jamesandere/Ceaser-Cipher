a222f1a989d16499b033dddfe8baec3870c40669
jamesandere <jamesandere56@gmail.com>
2019-04-06 15:53:08 +0300

    Create decryption test

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class CipherTest {
+@Test
+  public void CipherTest{
+  Cipher cipher = new Cipher();
+  assertEquals("car", cipher.decrypt("dbs",1));
+  }
 }
