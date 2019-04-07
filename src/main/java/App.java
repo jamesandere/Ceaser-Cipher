1d6ef21dd4ca8d73f1d8d198765dbd99336a9da7
jamesandere <jamesandere56@gmail.com>
2019-04-07 16:54:50 +0300

    Add method to print out encrypted text

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,34 +3,31 @@ import java.util.Scanner;
 
 public class App{
     public static void main(String[] args){
-//        Console myConsole = System.console();
         Scanner myObj = new Scanner(System.in);
-//        Encrypt encry = new Encrypt();
-//        Decrypt decry = new Decrypt();
         System.out.println("Welcome to Caesar Cipher!");
-//        String App = myConsole.readLine();
         System.out.println("***What's it all about?***");
         System.out.println("It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet.");
+        System.out.println("Please enter name to continue");
         String word = myObj.nextLine();
+        System.out.println("Welcome " + word);
         System.out.println("Enter 1 to encrypt");
         System.out.println("Enter 2 to decrypt");
         int pick = myObj.nextInt();
+        Caesar caes = new Caesar();
+        Cipher ciph = new Cipher();
         if(pick==1){
             System.out.println("Enter word to encrypt");
-//            String input = new String();
             String input = myObj.next();
             System.out.println("Enter key to shift letters");
             int move = myObj.nextInt();
-//            String resultEncryp = caesar.encrypt(input, move);
-//             System.out.println(resultEncryp);
+            System.out.println(caes.encrypt(input, move));
         }
         else if(pick==2){
             System.out.println("Enter word to decrypt");
             String input = myObj.next();
             System.out.println("Enter key to shift letters");
             int move = myObj.nextInt();
-//            String resultDecryp = cipher.decrypt(input, move);
-//            System.out.println(resultDecryp);
+            System.out.println(ciph.decrypt(input, move));
         }
         else {
             System.out.println("Invalid choice");
@@ -38,5 +35,3 @@ public class App{
     }
 }
 
-    }
-}
